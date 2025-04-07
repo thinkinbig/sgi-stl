74e03ffaba627c767157c40f629ae089670cef98
thinkinbig <lizeyu1101@gmail.com>
2025-04-07 23:40:50 +0200

    update namespace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,28 @@
+# CMake generated Testfile for 
+# Source directory: /home/lzy/sgi-stl/modern_sgi_stl
+# Build directory: /home/lzy/sgi-stl/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(mstl_alloc_test "/home/lzy/sgi-stl/build/bin/mstl_alloc_test")
+set_tests_properties(mstl_alloc_test PROPERTIES  ENVIRONMENT "CTEST_FULL_OUTPUT=1" LABELS "debug" WORKING_DIRECTORY "/home/lzy/sgi-stl/build/bin" _BACKTRACE_TRIPLES "/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;112;add_test;/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;0;")
+add_test(mstl_construct_test "/home/lzy/sgi-stl/build/bin/mstl_construct_test")
+set_tests_properties(mstl_construct_test PROPERTIES  ENVIRONMENT "CTEST_FULL_OUTPUT=1" LABELS "debug" WORKING_DIRECTORY "/home/lzy/sgi-stl/build/bin" _BACKTRACE_TRIPLES "/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;113;add_test;/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;0;")
+add_test(mstl_uninitialized_test "/home/lzy/sgi-stl/build/bin/mstl_uninitialized_test")
+set_tests_properties(mstl_uninitialized_test PROPERTIES  ENVIRONMENT "CTEST_FULL_OUTPUT=1" LABELS "debug" WORKING_DIRECTORY "/home/lzy/sgi-stl/build/bin" _BACKTRACE_TRIPLES "/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;114;add_test;/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;0;")
+add_test(mpthread_alloc_test "/home/lzy/sgi-stl/build/bin/mpthread_alloc_test")
+set_tests_properties(mpthread_alloc_test PROPERTIES  ENVIRONMENT "CTEST_FULL_OUTPUT=1" LABELS "debug" WORKING_DIRECTORY "/home/lzy/sgi-stl/build/bin" _BACKTRACE_TRIPLES "/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;115;add_test;/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;0;")
+add_test(mstl_iterator_test "/home/lzy/sgi-stl/build/bin/mstl_iterator_test")
+set_tests_properties(mstl_iterator_test PROPERTIES  ENVIRONMENT "CTEST_FULL_OUTPUT=1" LABELS "debug" WORKING_DIRECTORY "/home/lzy/sgi-stl/build/bin" _BACKTRACE_TRIPLES "/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;116;add_test;/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;0;")
+add_test(mstl_vector_test "/home/lzy/sgi-stl/build/bin/mstl_vector_test")
+set_tests_properties(mstl_vector_test PROPERTIES  ENVIRONMENT "CTEST_FULL_OUTPUT=1" LABELS "debug" WORKING_DIRECTORY "/home/lzy/sgi-stl/build/bin" _BACKTRACE_TRIPLES "/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;117;add_test;/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;0;")
+add_test(mstl_list_test "/home/lzy/sgi-stl/build/bin/mstl_list_test")
+set_tests_properties(mstl_list_test PROPERTIES  ENVIRONMENT "CTEST_FULL_OUTPUT=1" LABELS "debug" WORKING_DIRECTORY "/home/lzy/sgi-stl/build/bin" _BACKTRACE_TRIPLES "/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;118;add_test;/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;0;")
+add_test(mstl_deque_test "/home/lzy/sgi-stl/build/bin/mstl_deque_test")
+set_tests_properties(mstl_deque_test PROPERTIES  ENVIRONMENT "CTEST_FULL_OUTPUT=1" LABELS "debug" WORKING_DIRECTORY "/home/lzy/sgi-stl/build/bin" _BACKTRACE_TRIPLES "/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;119;add_test;/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;0;")
+add_test(mstl_stack_test "/home/lzy/sgi-stl/build/bin/mstl_stack_test")
+set_tests_properties(mstl_stack_test PROPERTIES  _BACKTRACE_TRIPLES "/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;120;add_test;/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;0;")
+add_test(mstl_queue_test "/home/lzy/sgi-stl/build/bin/mstl_queue_test")
+set_tests_properties(mstl_queue_test PROPERTIES  _BACKTRACE_TRIPLES "/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;121;add_test;/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;0;")
+add_test(mstl_heap_test "/home/lzy/sgi-stl/build/bin/mstl_heap_test")
+set_tests_properties(mstl_heap_test PROPERTIES  ENVIRONMENT "CTEST_FULL_OUTPUT=1" LABELS "debug" WORKING_DIRECTORY "/home/lzy/sgi-stl/build/bin" _BACKTRACE_TRIPLES "/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;122;add_test;/home/lzy/sgi-stl/modern_sgi_stl/CMakeLists.txt;0;")
